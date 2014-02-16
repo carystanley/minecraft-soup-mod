f4b0fc7d1f12cf18af3436865f3d295f864abed1
Cary Stanley <cary@yahoo-inc.com>
2014-02-15 19:38:22 -0800

    More Fix Up

--- java/com/carystanley/soup/common/Soup.java
+++ java/com/carystanley/soup/common/Soup.java
@@ -1,10 +1,15 @@
 package com.carystanley.soup.common;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+
 import com.carystanley.soup.common.item.SoupBase;
+
 import net.minecraft.util.RegistryNamespaced;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.GameData;
@@ -15,34 +20,37 @@ import net.minecraft.init.Items;
 @Mod(modid = "carystanley_soup", name = "Soup", version = "1.0")
 
 public class Soup {
-        public static final RegistryNamespaced itemRegistry = GameData.itemRegistry;
+	public static Item chickenNoodleSoup = (new SoupBase(9)).setUnlocalizedName("soup:chickenNoodleSoup").setTextureName("soup:chicken_noodle_soup").setCreativeTab(CreativeTabs.tabFood);
+	public static Item carrotSoup = (new SoupBase(9)).setUnlocalizedName("soup:carrotSoup").setTextureName("soup:carrot_soup").setCreativeTab(CreativeTabs.tabFood);
+	public static Item beefStew = (new SoupBase(9)).setUnlocalizedName("soup:beefStew").setTextureName("soup:beef_stew").setCreativeTab(CreativeTabs.tabFood);
+	public static Item potatoSoup = (new SoupBase(3)).setUnlocalizedName("soup:potatoSoup").setTextureName("soup:potato_soup").setCreativeTab(CreativeTabs.tabFood);
+	public static Item pumpkinSoup = (new SoupBase(1)).setUnlocalizedName("soup:pumpkinSoup").setTextureName("soup:pumpkin_soup").setCreativeTab(CreativeTabs.tabFood);
+	public static Item wontonSoup = (new SoupBase(11)).setUnlocalizedName("soup:wontonSoup").setTextureName("soup:wonton_soup").setCreativeTab(CreativeTabs.tabFood);
+	public static Item mulligatawnySoup = (new SoupBase(11)).setUnlocalizedName("soup:mulligatawnySoup").setTextureName("soup:mulligatawny_soup").setCreativeTab(CreativeTabs.tabFood);
+	public static Item splitPeaSoup = (new SoupBase(1)).setUnlocalizedName("soup:splitPeaSoup").setTextureName("soup:split_pea_soup").setCreativeTab(CreativeTabs.tabFood);
+	public static Item cactusSoup = (new SoupBase(1)).setUnlocalizedName("soup:cactusSoup").setTextureName("soup:cactus_soup").setCreativeTab(CreativeTabs.tabFood);
 
-	public static Item chickenNoodleSoup = (new SoupBase(9)).setUnlocalizedName("chickenNoodleSoup").setTextureName("soup:chicken_noodle_soup");
-	public static Item carrotSoup = (new SoupBase(9)).setUnlocalizedName("carrotSoup").setTextureName("soup:carrot_soup");
-	public static Item beefStew = (new SoupBase(9)).setUnlocalizedName("beefStew").setTextureName("soup:beef_stew");
-	public static Item potatoSoup = (new SoupBase(3)).setUnlocalizedName("potatoSoup").setTextureName("soup:potato_soup");
-	public static Item pumpkinSoup = (new SoupBase(1)).setUnlocalizedName("pumpkinSoup").setTextureName("soup:pumpkin_soup");
-	public static Item wontonSoup = (new SoupBase(11)).setUnlocalizedName("wontonSoup").setTextureName("soup:wonton_soup");
-	public static Item mulligatawnySoup = (new SoupBase(11)).setUnlocalizedName("mulligatawnySoup").setTextureName("soup:mulligatawny_soup");
-	public static Item splitPeaSoup = (new SoupBase(1)).setUnlocalizedName("splitPeaSoup").setTextureName("soup:split_pea_soup");
-	public static Item cactusSoup = (new SoupBase(1)).setUnlocalizedName("cactusSoup").setTextureName("soup:cactus_soup");
-
+	@EventHandler
+	public void preinit(FMLPreInitializationEvent event)
+	{
+        loadItems();
+	}
+	 
+	@EventHandler
 	public void init(FMLInitializationEvent event) {
-                loadItems();
 		loadRecipes();
-		loadLang();
 	}
 
 	public void loadItems() {
-		itemRegistry.addObject(5670, "chicken_noodle_soup", Soup.chickenNoodleSoup);    
-		itemRegistry.addObject(5671, "carrot_soup", Soup.carrotSoup);    
-		itemRegistry.addObject(5672, "beef_stew", Soup.beefStew);    
-		itemRegistry.addObject(5673, "potato_soup", Soup.potatoSoup);    
-		itemRegistry.addObject(5674, "pumpkin_soup", Soup.pumpkinSoup);    
-		itemRegistry.addObject(5675, "wonton_soup", Soup.wontonSoup);    
-		itemRegistry.addObject(5676, "mulligatawny_soup", Soup.mulligatawnySoup);    
-		itemRegistry.addObject(5677, "split_pea_soup", Soup.splitPeaSoup);    
-		itemRegistry.addObject(5678, "cactus_soup", Soup.cactusSoup);    
+		GameRegistry.registerItem(Soup.chickenNoodleSoup, "chicken_noodle_soup", "soup");    
+		GameRegistry.registerItem(Soup.carrotSoup, "carrot_soup", "soup");    
+		GameRegistry.registerItem(Soup.beefStew, "beef_stew", "soup");    
+		GameRegistry.registerItem(Soup.potatoSoup, "potato_soup", "soup");    
+		GameRegistry.registerItem(Soup.pumpkinSoup, "pumpkin_soup", "soup");    
+		GameRegistry.registerItem(Soup.wontonSoup, "wonton_soup", "soup");    
+		GameRegistry.registerItem(Soup.mulligatawnySoup, "mulligatawny_soup", "soup");    
+		GameRegistry.registerItem(Soup.splitPeaSoup, "split_pea_soup", "soup");    
+		GameRegistry.registerItem(Soup.cactusSoup, "cactus_soup", "soup");    
 	}
 	
 	public void loadRecipes() {
@@ -108,18 +116,6 @@ public class Soup {
 			Blocks.cactus,
 			Blocks.cactus
 		});
-	}
-	
-	public void loadLang() {
-		LanguageRegistry.addName(Soup.chickenNoodleSoup, "Chicken Noodle Soup");
-		LanguageRegistry.addName(Soup.carrotSoup, "Carrot Soup");
-		LanguageRegistry.addName(Soup.beefStew, "Beef Stew");
-		LanguageRegistry.addName(Soup.potatoSoup, "Potato Soup");
-		LanguageRegistry.addName(Soup.pumpkinSoup, "Pumpkin Soup");
-		LanguageRegistry.addName(Soup.wontonSoup, "Wonton Soup");
-		LanguageRegistry.addName(Soup.mulligatawnySoup, "Mulligatawny Soup");
-		LanguageRegistry.addName(Soup.splitPeaSoup, "Split Pea Soup");
-		LanguageRegistry.addName(Soup.cactusSoup, "Cactus Soup");
 	}
 
 }
